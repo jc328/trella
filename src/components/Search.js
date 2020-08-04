311ff1615d9e72a5c592570e5cd28e64da48d41a
John Chen <johnchen328@gmail.com>
2020-08-04 01:07:16 -0700

    added icons color, borders, spacing for navbar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
-function Search (props) {
+function Search () {
 
   return (
-    <TextField InputProps={{ style: { marginLeft: 10, fontSize: 10, height: 20, backgroundColor: 'lightblue'} }}
+    <TextField
+    InputProps={{
+      startAdornment: (
+        <InputAdornment position="start" >
+          <SearchOutlinedIcon style={{ fill: 'white' }}/>
+        </InputAdornment>
+      ),
+      style: {
+       marginLeft: 10,
+       fontSize: 10,
+       height: 20,
+       backgroundColor: 'lightblue',
+
+     }}
+    }
+    // label="search"
     variant="outlined"
     size="small"
-  />
+     />
     )
   }
+
 
 
 export default Search;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,10 +10,10 @@ function Navbar () {
     <div className="navbar">
       <div className="navbar_left">
         <IconButton onClick={console.log('Menu Item Button')}>
-        <AppsIcon />
+        <AppsIcon style={{ fill: 'white', backgroundColor: 'lightblue', height: 18, marginBottom: 4, borderRadius: 3}}/>
         </IconButton>
         <IconButton onClick={console.log('Home Button')}>
-        <HomeOutlinedIcon />
+        <HomeOutlinedIcon style={{ fill: 'white', backgroundColor: 'lightblue', height: 18, marginBottom: 4, borderRadius: 3}}/>
         </IconButton>
 
         <Button variant="contained" color="primary" size="small">Boards</Button>
